9e0b8751f5d64ab8de942628767385f3a32ac076
Abhay <krabhay476@gmail.com>
2025-01-17 03:29:57 +0530

    new0

--- backend/app.js
+++ backend/app.js
@@ -20,11 +20,12 @@ const path = require("path");
 __dirname = path.resolve();
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontned/build")));
-    app.get("*", (req, res)=>{
+    app.use(express.static(path.join(__dirname, "frontend", "build")));
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
     });
 }
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
